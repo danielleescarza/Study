c8a9734fd75faac936863a4e1f428012d6c0b0ef
lovedbydani <daescarza@gmail.com>
2025-06-05 17:58:25 +0800

    commit

--- accounts/models.py
+++ accounts/models.py
@@ -23,11 +23,31 @@ class CustomUser(AbstractUser):
         ("BSEd-English", "Bachelor of Science in Secondary Education Major in English"),
         ("College", "College Department"),
         ("Accounting", "Accounting Department"),
+        ("Office", "Office of the President and CEO, COO"),
+        ("Compliance", "Compliance Department"),
+        ("HumanResource", "Human Resource Department"),
+        ("HealthServices", "Health Services Department"),
+        ("ICT", "ICT Department"),
+        ("Guidance", "Guidance Department"),
+        ("Marketing", "Marketing Department"),
+        ("Procurement", "Procurement Department"),
+        ("Housekeeping", "Housekeeping Department"), 
+        ("Security", "Security Department"),
     )
     
     LEVEL_CHOICES = (
         ("1", "Grade 1"),
         ("2", "Grade 2"),
+        ("3", "Grade 3"),
+        ("4", "Grade 4"),
+        ("5", "Grade 5"),
+        ("6", "Grade 6"),
+        ("7", "Grade 7"),
+        ("8", "Grade 8"),
+        ("9", "Grade 9"),
+        ("10", "Grade 10"),
+        ("11", "Grade 11"),
+        ("12", "Grade 12"),
         ("first_year", "1st Year"),
         ("second_year", "2nd Year"),
         ("third_year", "3rd Year"),
@@ -47,5 +67,3 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.student_number})"
 
-
-# Create your models here.
--- accounts/views.py
+++ accounts/views.py
@@ -2,16 +2,26 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import CustomUserCreationForm
-from .models import CustomUser 
+from .models import CustomUser
+
 
 class Create(CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')  # Redirect to the login page after successful creation
+    success_url = reverse_lazy('login')
     template_name = 'registration/create.html'
-    
+
     def form_valid(self, form):
-        # Set the username to the student_number before saving
         user = form.save(commit=False)
-        user.username = str(form.cleaned_data['student_number'])
-        user.save()
+        user.username = str(form.cleaned_data['student_number'])  # Set username to student number
+
+        # Set user permissions based on user type
+        user_type = form.cleaned_data['user_type']
+        if user_type == 'admin':
+            user.is_superuser = True
+            user.is_staff = True
+        else:
+            user.is_superuser = False
+            user.is_staff = True
+
+        user.save()  # Save the user instance
         return super().form_valid(form)
